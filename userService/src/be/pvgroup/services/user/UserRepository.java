692319a370d3b127a6bbfa266c2de8c50f686709
Bert Godon <bert.godon@gmail.com>
2016-03-19 16:50:11 +0100

    Delete UserRepository.java

--- userService/src/be/pvgroup/services/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package be.pvgroup.services.user;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String>{
-
-	public User findById(Long id);
-}
