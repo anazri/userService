a57afb43d613af929a8c6226fac4f575499a242e
Bert Godon <bert.godon@gmail.com>
2016-03-19 17:34:43 +0100

    Update build.sh

--- userService/docker/build.sh
+++ userService/docker/build.sh
@@ -1,3 +1,7 @@
-
+echo "start mongo database"
+docker run -P -d --name mongodb mongo 
 cp ../target/userService-0.0.1-SNAPSHOT.jar .
 docker build -t devoxx/userService .
+
+docker run -d -p 8080:8080 -e SPRING_DATA_MONGODB_URI=mongodb://192.168.59.103/users --name devoxx/userService devoxx/userService
+docker logs devoxx/userService
