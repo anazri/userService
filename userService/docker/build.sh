c58f663ee26e601b43cf16338ed6fe1cf90a0d06
Bert Godon <bert.godon@gmail.com>
2016-03-19 17:39:40 +0100

    Update build.sh

--- userService/docker/build.sh
+++ userService/docker/build.sh
@@ -1,4 +1,8 @@
 echo "start mongo database"
+DOCKER_HOST=tcp://192.168.99.100:2376
+DOCKER_MACHINE_NAME=default
+DOCKER_TLS_VERIFY=1
+DOCKER_CERT_PATH=/Users/[username]/.docker/machine/machines/default
 docker run -P -d --name mongodb mongo 
 cp ../target/userService-0.0.1-SNAPSHOT.jar .
 docker build -t devoxx/userService .
